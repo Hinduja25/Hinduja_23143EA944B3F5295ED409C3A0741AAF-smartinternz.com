be47e447abe303fb7cdd9b55773b71153b33b580
Hinduja25 <144442886+Hinduja25@users.noreply.github.com>
2023-10-26 06:09:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+implement a class called bank account that represents a bank account the class should have private
+class bankaccount:
+  def __init__(self, account_number,account_holder_name,initial_balace=0.0):
+    self.__account_number=account
+      _number
+    self.__account_holder_name=
+      account_holder_name
+    self.__account_balance=initial
+       _balance
+  def deposit(self,amount):
+    if amount>0:
+      self.__account_balace+=amount
+      print("deposited {}.new balance: {}".format(amount,self.__account_balance))
+    else:
+      print("invalid withdrawal amount or insufficient balance:")
+  def display_balance(self):
+    print("amount balance for {}(account #{}): {}".format(
+      self.__account _holder_name.self__account_number,
+      self.__account_balance))
+    
+    
